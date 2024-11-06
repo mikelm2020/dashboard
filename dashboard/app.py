e91a8cbe1d205202fa75bceb2b3f088d9409a196
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2024-11-05 20:09:44 -0600

    Added gross profit margin metric

--- dashboard/app.py
+++ dashboard/app.py
@@ -36,9 +36,8 @@ def fetch_scalar_data(endpoint, month, year):
 
 
 # Función para obtener datos del API
-def fetch_dashboard_data(endpoint, month, year):
-    params = {"month": month, "year": year}
-    response = requests.get(f"{base_url}/{endpoint}", params=params)
+def fetch_dashboard_data(endpoint):
+    response = requests.get(f"{base_url}/{endpoint}")
     data = response.json()
     return pd.DataFrame(data)  # Convierte el listado de datos en un DataFrame
 
@@ -52,6 +51,59 @@ def calculate_delta(previous_value, last_value, divisor: int):
         * 100,
         2,
     )
+
+
+def get_delta(
+    number_month: int,
+    number_year: int,
+    concept_array,
+    mount_column: str,
+    last_value,
+    divisor: int,
+    delta_type: str,
+):
+    delta = None
+    concept_array_filtered_previous = None
+
+    if number_year == initial_year:
+        if number_month is not None:
+            if number_month > 1:
+                concept_array_filtered_previous = concept_array[
+                    (concept_array["month_concept"] == number_month - 1)
+                    & (concept_array["year_concept"] == number_year)
+                ]
+    else:
+        if number_month is None:
+            concept_array_filtered_previous = concept_array[
+                concept_array["year_concept"] == number_year - 1
+            ]
+        else:
+            if number_month == 1:
+                concept_array_filtered_previous = concept_array[
+                    (concept_array["month_concept"] == 12)
+                    & (concept_array["year_concept"] == number_year - 1)
+                ]
+            else:
+                concept_array_filtered_previous = concept_array[
+                    (concept_array["month_concept"] == number_month - 1)
+                    & (concept_array["year_concept"] == number_year)
+                ]
+        if delta_type == "sum":
+            previous_value = concept_array_filtered_previous[mount_column].sum()
+        elif delta_type == "mean":
+            previous_value = concept_array_filtered_previous[mount_column].mean()
+        elif delta_type == "median":
+            previous_value = concept_array_filtered_previous[mount_column].median()
+        elif delta_type == "max":
+            previous_value = concept_array_filtered_previous[mount_column].max()
+        elif delta_type == "min":
+            previous_value = concept_array_filtered_previous[mount_column].min()
+        elif delta_type == "count":
+            previous_value = concept_array_filtered_previous[mount_column].count()
+
+        delta = calculate_delta(previous_value, last_value, divisor)
+
+        return delta
 
 
 def get_metric(title: str, mount_df, metric_label: str, delta_df, divisor: int):
@@ -79,9 +131,12 @@ def format_top_dataframe(
     mount_column: str,
     subject_label: str,
     mount_label: str,
+    is_mount_format: bool = True,
 ):
     formated_df = st.dataframe(
-        df.style.format(formatter={mount_column: "$ {:,.2f}"}),
+        df.style.format(
+            formatter={mount_column: "$ {:,.2f}" if is_mount_format else "{:,.0f}"}
+        ),
         hide_index=True,
         column_config={
             subject_column: st.column_config.TextColumn(label=subject_label),
@@ -118,7 +173,7 @@ if month == "Todos":
 base_url = os.getenv("BASE_URL")
 
 # Obtiene todas las ventas del sistema
-sales_array = fetch_dashboard_data("sales", month, year)
+sales_array = fetch_dashboard_data("sales")
 if sales_array is not None:
     # Obtiene las ventas del año seleccionado
     # Obtiene encabezados y calcula las metricas de ventas
@@ -151,7 +206,7 @@ else:  # Si no hay datos, muestra un mensaje de advertencia
 
 
 # Obtiene todas las compras del sistema
-purchases_array = fetch_dashboard_data("purchases", month, year)
+purchases_array = fetch_dashboard_data("purchases")
 if purchases_array is not None:
     # Obtiene las ventas del año seleccionado
     # Obtiene encabezados y calcula las metricas de ventas
@@ -186,16 +241,77 @@ else:  # Si no hay datos, muestra un mensaje de advertencia
     st.warning("No hay datos de compras disponibles")
 
 
+# Obtiene todas las ventas por vendedor del sistema
+sales_by_seller_array = fetch_dashboard_data("sellers")
+if sales_by_seller_array is not None:
+    # Obtiene las ventas del año seleccionado
+    # Obtiene encabezados
+    if month is None:
+        title_header = f"Ventas del año {year}"
+        # Filtra las filas en las que el año coincide
+        sales_by_seller_array_filtered = sales_by_seller_array[
+            sales_by_seller_array["year_concept"] == year
+        ]
+        total_sales_by_seller = sales_by_seller_array_filtered["total_sales"].sum()
+    else:
+        title_header = f"Ventas del mes del año {year}"
+        # Filtra filas en las que el mes y el año coinciden
+        sales_by_seller_array_filtered = sales_by_seller_array[
+            (sales_by_seller_array["month_concept"] == month)
+            & (sales_by_seller_array["year_concept"] == year)
+        ]
+        total_sales_by_seller = sales_by_seller_array_filtered["total_sales"].sum()
+
+# Obtiene todas las ventas por producto del sistema
+sales_by_product_array = fetch_dashboard_data("products")
+if sales_by_product_array is not None:
+    # Obtiene las ventas del año seleccionado
+    # Obtiene encabezados
+    if month is None:
+        title_header = f"Volumen de Ventas del año {year}"
+        # Filtra las filas en las que el año coincide
+        sales_by_product_array_filtered = sales_by_product_array[
+            sales_by_product_array["year_concept"] == year
+        ]
+        total_qty_by_product = sales_by_product_array_filtered["total_qty"].sum()
+    else:
+        title_header = f"Volumen de Ventas del mes del año {year}"
+        # Filtra filas en las que el mes y el año coinciden
+        sales_by_product_array_filtered = sales_by_product_array[
+            (sales_by_product_array["month_concept"] == month)
+            & (sales_by_product_array["year_concept"] == year)
+        ]
+        total_qty_by_product = sales_by_product_array_filtered["total_qty"].sum()
+
+# Obtiene el margen bruto de ganancia
+gross_profit_margin_array = fetch_dashboard_data("gross-profit-margin")
+if gross_profit_margin_array is not None:
+    # Obtiene las ventas del año seleccionado
+    # Obtiene encabezados
+    if month is None:
+        title_header = f"Margen de Ganancia Bruta del año {year}"
+        # Filtra las filas en las que el año coincide
+        gross_profit_margin_array_filtered = gross_profit_margin_array[
+            gross_profit_margin_array["year_concept"] == year
+        ]
+        total_gpm = gross_profit_margin_array_filtered["total_gpm"].sum()
+    else:
+        title_header = f"Volumen de Ventas del mes del año {year}"
+        # Filtra filas en las que el mes y el año coinciden
+        gross_profit_margin_array_filtered = gross_profit_margin_array[
+            (gross_profit_margin_array["month_concept"] == month)
+            & (gross_profit_margin_array["year_concept"] == year)
+        ]
+        total_gpm = gross_profit_margin_array_filtered["total_gpm"].sum()
+
 # Crea tabs
-tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
     [
         "Inicio",
         "Clientes",
         "Proveedores",
         "Vendedores",
         "Productos",
-        "Ventas",
-        "Compras",
     ]
 )
 
@@ -210,55 +326,33 @@ with tab1:
     delta_number_of_sales = None
     sales_array_filtered_previous = None
 
-    if year == initial_year:
-        if month is not None:
-            if month > 1:
-                sales_array_filtered_previous = sales_array[
-                    (sales_array["month_concept"] == month - 1)
-                    & (sales_array["year_concept"] == year)
-                ]
-    else:
-        if month is None:
-            sales_array_filtered_previous = sales_array[
-                sales_array["year_concept"] == year - 1
-            ]
-        else:
-            if month == 1:
-                sales_array_filtered_previous = sales_array[
-                    (sales_array["month_concept"] == 12)
-                    & (sales_array["year_concept"] == year - 1)
-                ]
-            else:
-                sales_array_filtered_previous = sales_array[
-                    (sales_array["month_concept"] == month - 1)
-                    & (sales_array["year_concept"] == year)
-                ]
+    delta_sales = get_delta(
+        month, year, sales_array, "total_sales", total_sales, 1000, "sum"
+    )
 
-        previous_total_sales = sales_array_filtered_previous["total_sales"].sum()
-        delta_sales = calculate_delta(previous_total_sales, total_sales, 1000)
+    delta_average_sales = get_delta(
+        month, year, sales_array, "total_sales", average_sales, 1000, "mean"
+    )
 
-        previous_average_sales = sales_array_filtered_previous["total_sales"].mean()
-        delta_average_sales = calculate_delta(
-            previous_average_sales, average_sales, 1000
-        )
+    delta_median_sales = get_delta(
+        month, year, sales_array, "total_sales", median_sales, 1000, "median"
+    )
 
-        previous_median_sales = sales_array_filtered_previous["total_sales"].median()
-        delta_median_sales = calculate_delta(previous_median_sales, median_sales, 1000)
+    delta_max_sales = get_delta(
+        month, year, sales_array, "total_sales", max_sales, 1000, "max"
+    )
 
-        previous_max_sales = sales_array_filtered_previous["total_sales"].max()
-        delta_max_sales = calculate_delta(previous_max_sales, max_sales, 1000)
+    delta_min_sales = get_delta(
+        month, year, sales_array, "total_sales", min_sales, 1000, "min"
+    )
 
-        previous_min_sales = sales_array_filtered_previous["total_sales"].min()
-        delta_min_sales = calculate_delta(previous_min_sales, min_sales, 1000)
-
-        previous_number_of_sales = sales_array_filtered_previous["total_sales"].count()
-        delta_number_of_sales = calculate_delta(
-            previous_number_of_sales, number_of_sales, 1
-        )
+    delta_number_of_sales = get_delta(
+        month, year, sales_array, "total_sales", number_of_sales, 1, "count"
+    )
 
     col1, col2 = st.columns(2)
     with col1:
-        get_metric("Ventas", total_sales, "mdp", delta_sales, 1000)
+        get_metric("Ingresos Netos", total_sales, "mdp", delta_sales, 1000)
     with col2:
         get_metric("Ventas Promedio", average_sales, "mdp", delta_average_sales, 1000)
 
@@ -273,7 +367,7 @@ with tab1:
         get_metric("Minima Venta", min_sales, "mdp", delta_min_sales, 1000)
     with col6:
         get_metric(
-            "Número de ventas", number_of_sales, "facturas", delta_number_of_sales, 1
+            "Volumen de ventas", number_of_sales, "facturas", delta_number_of_sales, 1
         )
 
     # Calcula métricas de compras
@@ -285,71 +379,35 @@ with tab1:
     delta_number_of_purchases = None
     purchases_array_filtered_previous = None
 
-    if year == initial_year:
-        if month is not None:
-            if month > 1:
-                purchases_array_filtered_previous = purchases_array[
-                    (purchases_array["month_concept"] == month - 1)
-                    & (purchases_array["year_concept"] == year)
-                ]
-    else:
-        if month is None:
-            purchases_array_filtered_previous = purchases_array[
-                purchases_array["year_concept"] == year - 1
-            ]
-        else:
-            if month == 1:
-                purchases_array_filtered_previous = purchases_array[
-                    (purchases_array["month_concept"] == 12)
-                    & (purchases_array["year_concept"] == year - 1)
-                ]
-            else:
-                purchases_array_filtered_previous = purchases_array[
-                    (purchases_array["month_concept"] == month - 1)
-                    & (purchases_array["year_concept"] == year)
-                ]
+    delta_purchases = get_delta(
+        month, year, purchases_array, "total_purchases", total_purchases, 1000, "sum"
+    )
 
-        previous_total_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].sum()
-        delta_purchases = calculate_delta(
-            previous_total_purchases, total_purchases, 1000
-        )
+    delta_average_purchases = get_delta(
+        month, year, purchases_array, "total_purchases", average_purchases, 1000, "mean"
+    )
 
-        previous_average_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].mean()
-        delta_average_purchases = calculate_delta(
-            previous_average_purchases, average_purchases, 1000
-        )
+    delta_median_purchases = get_delta(
+        month,
+        year,
+        purchases_array,
+        "total_purchases",
+        median_purchases,
+        1000,
+        "median",
+    )
 
-        previous_median_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].median()
-        delta_median_purchases = calculate_delta(
-            previous_median_purchases, median_purchases, 1000
-        )
+    delta_max_purchases = get_delta(
+        month, year, purchases_array, "total_purchases", max_purchases, 1000, "max"
+    )
 
-        previous_max_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].max()
-        delta_max_purchases = calculate_delta(
-            previous_max_purchases, max_purchases, 1000
-        )
+    delta_min_purchases = get_delta(
+        month, year, purchases_array, "total_purchases", min_purchases, 1000, "min"
+    )
 
-        previous_min_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].min()
-        delta_min_purchases = calculate_delta(
-            previous_min_purchases, min_purchases, 1000
-        )
-
-        previous_number_of_purchases = purchases_array_filtered_previous[
-            "total_purchases"
-        ].count()
-        delta_number_of_purchases = calculate_delta(
-            previous_number_of_purchases, number_of_purchases, 1
-        )
+    delta_number_of_purchases = get_delta(
+        month, year, purchases_array, "total_purchases", number_of_purchases, 1, "count"
+    )
 
     col7, col8 = st.columns(2)
     with col7:
@@ -372,13 +430,30 @@ with tab1:
         get_metric("Minima Compra", min_purchases, "mdp", delta_min_purchases, 1000)
     with col12:
         get_metric(
-            "Número de Compras",
+            "Volumen de Compras",
             number_of_purchases,
             "compras",
             delta_number_of_purchases,
             1,
         )
 
+    # Calcula métricas
+    delta_gross_profit_margin = None
+    sales_array_filtered_previous = None
+
+    delta_gross_profit_margin = get_delta(
+        month, year, gross_profit_margin_array, "total_gpm", total_gpm, 1000, "sum"
+    )
+
+    col13, col14 = st.columns(2)
+    with col13:
+        get_metric(
+            "Margen de Ganancia Bruta",
+            total_gpm,
+            "mdp",
+            delta_gross_profit_margin,
+            1000,
+        )
 
 with tab2:
     # Obtener total de ventas
@@ -425,3 +500,49 @@ with tab3:
     with right_section:
         with st.popover("Mostrar gráfica"):
             get_circular_graph(top_providers, "total_purchases", "name")
+
+with tab4:
+    # Obtener total de ventas por vendedor
+    if total_sales_by_seller is not None:
+        st.header(f"{title_header} $ {total_sales_by_seller:,.2f}")
+    else:
+        st.warning("No hay datos de ventas disponibles")
+
+    st.header("Los 5 primeros mejores vendedores")
+    # Mostrar Top 5 Vendedores
+    top_sellers = get_top(sales_by_seller_array_filtered, "name", "total_sales")
+
+    left_section, right_section = st.columns(2)
+    if top_sellers.empty:
+        st.warning("No hay datos para mostrar")
+    else:
+        with left_section:
+            df_formated = format_top_dataframe(
+                top_sellers, "name", "total_sales", "Vendedor", "Ventas"
+            )
+    with right_section:
+        with st.popover("Mostrar gráfica"):
+            get_circular_graph(top_sellers, "total_sales", "name")
+
+with tab5:
+    # Obtener total de ventas por producto
+    if total_qty_by_product is not None:
+        st.header(f"{title_header}  {total_qty_by_product:,.0f}")
+    else:
+        st.warning("No hay datos de ventas disponibles")
+
+    st.header("Los 5 primeros mejores productos")
+    # Mostrar Top 5 Productos
+    top_products = get_top(sales_by_product_array_filtered, "name", "total_qty")
+
+    left_section, right_section = st.columns([0.7, 0.3])
+    if top_products.empty:
+        st.warning("No hay datos para mostrar")
+    else:
+        with left_section:
+            df_formated = format_top_dataframe(
+                top_products, "name", "total_qty", "Producto", "Cantidad", False
+            )
+    with right_section:
+        with st.popover("Mostrar gráfica"):
+            get_circular_graph(top_products, "total_qty", "name")
